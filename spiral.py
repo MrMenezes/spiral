22849040dc0b89f72ef237a20fe104dd486eeddc
Erick Menezes <erick.menezes@cubos.io>
2022-04-08 07:35:52 -0300

    feat: added animation and modfier rollback

--- spiral.py
+++ spiral.py
@@ -3,12 +3,13 @@ from math import atan2, cos, pi, radians, sin, sqrt
 class StopError(Exception):
     pass
 
-
 class GeometricForm():
-    def __init__(self, arestas, MINIMAL, tax):
+    def __init__(self, arestas, MINIMAL, tax, points):
         self.arestas = arestas
         self.__minimal__ = MINIMAL
         self.__tax__ = tax
+        self.points = points
+        self.sub_geometric = list()
 
     def newForm(self):
         arestas = list()
@@ -37,6 +38,30 @@ class GeometricForm():
             arestas.append(ArestaReta(dist, dist_t,self.__minimal__,self.__tax__))
             dist = dist_t
         return GeometricForm(arestas,self.__minimal__,self.__tax__)
+    
+    def spiral(self):
+        arestas = list()
+        x = self.arestas[len(self.arestas)-1].x2
+        y = self.arestas[len(self.arestas)-1].y2
+        dist = (x, y)
+        for key, value in enumerate(self.arestas):
+            if key + 1 > len(self.arestas) - 1:
+                final = arestas[0]
+            else:
+                final = self.arestas[key+1]
+            dist_t = final.dist()
+            starts = self.arestas[key].starts()
+            arestas.append(ArestaReta(dist, dist_t,self.__minimal__,self.__tax__, move_list=final.dist_move(), starts=starts))
+            dist = dist_t
+        self.sub_geometric.append(GeometricForm(arestas,self.__minimal__,self.__tax__, self.points))
+        for sub_geo in self.sub_geometric:
+            sub_geo.spiral()
+
+    def display(self, stroke, line, index=None):
+        for aresta in self.arestas:
+            aresta.display(stroke, line, index=index)
+        for sub_geo in self.sub_geometric:
+            sub_geo.display(stroke, line, index=index)
 
     def __str__(self):
         print("Coordenadas:\n")
@@ -46,22 +71,68 @@ class GeometricForm():
 
 
 class ArestaReta:
-    def __init__(self, start, end, MINIMAL, tax):
+    def __init__(self, start, end, MINIMAL=10, tax=0.2, color='black', move_list=list(), starts=list()):
         self.x1 = start[0]
         self.y1 = start[1]
+        self.movex = move_list[0]
+        self.movey = move_list[1]
         self.x2 = end[0]
         self.y2 = end[1]
+        self.endx = self.x2
+        self.endy = self.y2
         self.tax = tax
+        self.color = color
+        self.lines = []
+        if len(starts) == 0:
+            for i in range(4):
+                starts.append([self.x1, self.y1])
+        self.lines.append([self.x1, self.y1, self.endx, self.endy])
+        delta_x = (self.endx - self.movex)/2
+        delta_y  = (self.endy - self.movey)/2
+        self.lines.append([starts[1][0], starts[1][1], self.movex - delta_x, self.movey - delta_y])
+        self.lines.append([starts[2][0], starts[2][1], self.movex, self.movey])
+        self.lines.append([starts[3][0], starts[3][1], self.movex + delta_x, self.movey + delta_y])
+        self.state = 0
         if self.distance() < MINIMAL:
             raise StopError()
-
+    
     def dist(self):
-        x = self.x2
-        y = self.y2
-        for i in range(self.tax):
-            x = (self.x1+x) / (2)
-            y = (self.y1+y) / (2)
+        # print("dist x1: {} - y1: {} - tax{}".format(self.x1, self.y1,self.tax))
+        # print("dist x2: {} - y2: {} - tax{}".format(self.x2, self.y2,self.tax))
+        x = self.x1 + ((self.x2-self.x1) * self.tax)
+        y = self.y1 + ((self.y2-self.y1) * self.tax)
+        # print("dist x: {} - y: {}".format(x, y))
         return (x, y)
+        
+    def dist_move(self):
+        # print("Meio x1: {} - y1: {} - tax{}".format(self.x1, self.x2,self.tax))
+        x = self.x1 + ((self.x2-self.x1) * (self.tax / 2))
+        y = self.y1 + ((self.y2-self.y1) * (self.tax / 2))
+        # print("Meio x1: {} - y1: {}".format(x, y))
+        return (x, y)
+
+    def starts(self):
+        first = self.dist()
+        mid = self.dist_move()
+        delta_x = (first[0] - mid[0])/2
+        delta_y  = (first[1] - mid[1])/2
+        sec = (mid[0] - delta_x, mid[1] - delta_y)
+        last = (mid[0] + delta_x, mid[1] + delta_y)
+        return [first, sec, mid, last]
+
+    def display(self, stroke, line, index=None):
+        if index is not None:
+            line(*self.lines[index])
+            return
+        # print("display x1: {} - y1: {} - tax{}".format(self.x1, self.y1,self.tax))
+        # print("display x2: {} - y2: {} - tax{}".format(self.endx, self.endy,self.tax))
+        stroke(self.color)
+        line(*self.line_generator())
+    
+    def line_generator(self):
+        data = self.lines[self.state]   
+        self.state = 0 if self.state == len(self.lines) -1 else self.state + 1
+        return data
 
     def __str__(self):
         return"X1 {} - Y1 {}, X2 {} - Y2 {}\n".format(self.x1,
--- spiral2.py
+++ spiral2.py
@@ -1,63 +1,82 @@
 from pyp5js import *
-from spiral import ArestaReta, GeometricForm, StopError
-
+from spiral_base import ArestaReta, GeometricForm, StopError
+index = None
 points = list()
 last_points = list()
 geometric = list()
-
+geometrics = list()
+arestas = list()
 
 def setup():
-    global geometric
-    minimal = 10
-    tax = 4
     createCanvas(1024, 1024)
     background(160)
-    frameRate(30)
+    frameRate(12)
 
 
 def keyReleased():
     global geometric
+    global geometrics
     global points
     global last_points
+    global index
+    global arestas
+    
+    if key == 'p':
+        print(geometrics)
+        print(arestas)
     if key == 'a':
+        background(160)
         desenha()
         last_points = list()
     if key == 'z':
-        size = len(last_points)
-        if size > 1:
-            stroke(160)
-            line(last_points[size - 2][0], last_points[size - 2][1],
-                 last_points[size - 1][0], last_points[size - 1][1])
-            line(last_points[size - 2][0], last_points[size - 2][1],
-                 last_points[size - 1][0], last_points[size - 1][1])
-            line(last_points[size - 2][0], last_points[size - 2][1],
-                 last_points[size - 1][0], last_points[size - 1][1])
-            line(last_points[size - 2][0], last_points[size - 2][1],
-                 last_points[size - 1][0], last_points[size - 1][1])
-            line(last_points[size - 2][0], last_points[size - 2][1],
-                 last_points[size - 1][0], last_points[size - 1][1])
-            line(last_points[size - 2][0], last_points[size - 2][1],
-                 last_points[size - 1][0], last_points[size - 1][1])
+        if len(last_points) > 0:
             last_points.pop()
-        points.pop()
+        else:
+            points.pop()
+            geometrics.pop()
+            arestas.pop()
+        background(160)
     if key == 'd':
+        geometrics.append(geometric)
         geometric = list()
         points = list()
         last_points = list()
-    if key == 's':
+    if key == '-':
+        geometrics = list()
         geometric = list()
         points = list()
+        arestas = list()
         last_points = list()
         background(160)
-
-
+    if key ==0:
+        index = 0
+    if key ==1:
+        index = 1
+    if key ==2:
+        index = 2
+    if key ==3:
+        index = 3
+    if key == 4:
+        index = None
+       
 def draw():
-    global geometric
-    if len(geometric) > 1:
-        for aresta in geometric.pop(0):
-            stroke('black')
-            line(aresta.x1, aresta.y1, aresta.x2, aresta.y2)
-
+    global index
+    global last_points
+    if len(geometrics) > 0:
+        background(160)
+        for geometric in geometrics:
+            for geo in geometric:
+                geo.display(stroke, line, index)
+        for aresta in arestas:
+            for a in aresta:
+                a.display(stroke, line, 0)
+    size = len(last_points)
+    if size > 1:
+        stroke('red')
+        for last in range(size-1):
+            print(last_points)
+            line(last_points[last][0], last_points[last][1],
+                last_points[last + 1][0], last_points[last + 1][1])
 
 def mouseClicked():
     global points
@@ -66,19 +85,17 @@ def mouseClicked():
     py = mouseY
     points.append((px, py))
     last_points.append((px, py))
-    size = len(last_points)
-    if size > 1:
-        stroke('red')
-        line(last_points[size - 2][0], last_points[size - 2][1],
-             last_points[size - 1][0], last_points[size - 1][1])
+    
 
 
 def desenha():
     global points
     global geometric
+    global arestas
+    global last_points
     minimal = 10
-    tax = 4
-    arestas = list()
+    tax = 0.1
+    arestas_draw = list()
     x = points[0][0]
     y = points[0][1]
     dist = (x, y)
@@ -87,12 +104,16 @@ def desenha():
             dist_t = (x, y)
         else:
             dist_t = (points[key+1][0], points[key+1][1])
-        arestas.append(ArestaReta(dist, dist_t, minimal, tax))
+        ares = ArestaReta(dist, dist_t, minimal, tax)
+        arestas_draw.append(ares)
         dist = dist_t
-    q1 = GeometricForm(arestas, minimal, tax)
-    while 1:
-        try:
-            geometric.append(q1.arestas)
-            q1 = q1.newForm()
-        except StopError:
-            break
+    
+    arestas.append(arestas_draw)
+    q1 = GeometricForm(arestas_draw, minimal, tax, points=len(points))
+    q1.display(stroke, line)
+    try:
+        q1.spiral(),
+    except StopError:
+        pass
+    geometric.append(q1)
+    last_points = []
